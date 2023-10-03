f77efb9de178713da32653cf3a3fafa5c9c184aa
e1b21064 <e1b21064@oit.ac.jp>
2023-10-03 14:44:24 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/table3/lec02/Lec02Application.java
@@ -0,0 +1,13 @@
+package oit.is.table3.lec02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/table3/lec02/Lec02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.table3.lec02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
